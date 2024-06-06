37d6c874fed144df0c8c81609383d1880be7e6f1
JayJetta1 <j_jenkins1@u.pacific.edu>
2024-06-05 21:38:13 -0700

    modified JS and html

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,12 +1,23 @@
-const userName = "user-name";
-const blogTitle = "blog-title";
-const blogContent = "blog-content";
+const userName = document.querySelector("#user-name");
+const blogTitle = document.querySelector("#blog-title");
+const blogContent = document.querySelector("#blog-content");
+const submitButton = document.querySelector("#submit-button");
 
-const user = {
-  name: "",
-  title: "",
-  content: "",
-};
+let info = localStorage.getItem("info");
 
-const userData = JSON.stringify(user);
-localStorage.setItem("user", "userData");
+submitButton.addEventListener("click", function () {
+  if (userName > 0 && blogTitle > 0 && blogContent > 0) {
+    localStorage.setItem("info", info);
+  } else {
+    return false;
+  }
+});
+
+// const user = {
+//   userNameInput: " ",
+//   blogTitleInput: " ",
+//   blogContentInput: " ",
+// };
+
+// const userData = JSON.stringify(user);
+// localStorage.setItem("user", "userData");
